a5de4e9fe3329fdfcf783094d4ddf2baa1ed15d4
ashishawasthi <ashish.awasthi@gmail.com>
2014-12-18 19:02:38 +0800

    basic sample project

---
+++ app/controllers/Application.java
@@ -0,0 +1,29 @@
+package controllers;
+
+import models.User;
+import play.*;
+import play.mvc.*;
+import play.libs.Json;
+import views.html.*;
+import play.data.Form;
+import play.db.ebean.Model;
+import java.util.*;
+
+public class Application extends Controller {
+
+    public static Result index() {
+        return ok(index.render("Message to Index"));
+    }
+
+    public static Result addUser() {
+    	User newUser = Form.form(User.class).bindFromRequest().get();
+    	newUser.save();
+    	return redirect(routes.Application.index());
+    }
+    
+    public static Result listUsers() {
+    	List<User> users = new Model.Finder(String.class, User.class).all();
+    	return ok(Json.toJson(users));
+    }
+
+}
---
+++ app/models/User.java
@@ -0,0 +1,27 @@
+package models;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+import play.data.validation.Constraints.Required;
+import play.db.ebean.Model;
+
+@Entity
+public class User extends Model {
+	@Id
+	private String id;
+	//@Required
+	private String fullName;
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getFullName() {
+		return fullName;
+	}
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
+	}
+}
